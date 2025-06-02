e1baf27483e295ee7594c25457598af0ab65ae75
Gavin <167050397+imhubgitting@users.noreply.github.com>
2025-06-02 17:01:00 -0400

    Create App.jsx

---
+++ part1/courseinfo/1.5/App.jsx
@@ -0,0 +1,55 @@
+// Gavin Antonacci 6/2/25 
+const Header = ({course}) => {
+  return <h1>{course}</h1>
+}
+
+const Content = ({parts}) => {
+  return (
+    <div>
+      <Part name ={parts[1].name} exercises={parts[1].exercises}/>
+      <Part name ={parts[2].name} exercises={parts[2].exercises}/>
+      <Part name ={parts[3].name} exercises={parts[3].exercises}/>
+    </div>
+  )
+}
+
+const Total = ({parts}) => {
+  return <p>Number of exercises {parts[1].exercises + parts[2].exercises + parts[3].exercises}</p>
+}
+
+const Part = ({name, exercises}) => {
+  return <p>{name} {exercises}</p>
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        NaN
+      },
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course = {course.name} />
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
+    </div>
+  )
+}
+
+export default App
